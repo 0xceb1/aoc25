c9eecead5a8365ebd3c907b1a58edf8b31bbfb14
Darren Weng <wjhdarren@gmail.com>
2025-12-02 13:23:46 +0800

    add template file

---
+++ src/template.rs
@@ -0,0 +1,15 @@
+use std::{error::Error, fs::File, io, io::prelude::*};
+
+pub fn solve() -> Result<(), Box<dyn Error>> {
+    part1()?;
+    part2()?;
+    Ok(())
+}
+
+fn part1() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+fn part2() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
